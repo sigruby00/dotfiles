1fa226ee1b610bcfcb4ba91390e3925683eaac32
sigruby <sigruby00@gmail.com>
2025-09-12 06:42:53 +0900

    add vimtex

--- .config/nvim/lua/ruby/plugins/vimtex.lua
+++ .config/nvim/lua/ruby/plugins/vimtex.lua
@@ -5,7 +5,9 @@ return {
     "folke/which-key.nvim",
   },
   init = function()
-    vim.g.vimtex_view_method = "zathura"
+    vim.g.vimtex_view_method = "skim"
+    vim.g.vimtex_view_skim_sync = 1
+    vim.g.vimtex_view_skim_activate = 1
 
     vim.keymap.set("n", "<leader>tt", function()
       require("which-key").show("\\")
