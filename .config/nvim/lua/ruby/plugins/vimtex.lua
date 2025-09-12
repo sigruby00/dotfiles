e87843af43d6af1e6916e7b6131fd2370075f0b9
sigruby <sigruby00@gmail.com>
2025-09-12 20:11:00 +0900

    add vimtex warning off

--- .config/nvim/lua/ruby/plugins/vimtex.lua
+++ .config/nvim/lua/ruby/plugins/vimtex.lua
@@ -8,6 +8,7 @@ return {
     vim.g.vimtex_view_method = "skim"
     vim.g.vimtex_view_skim_sync = 1
     vim.g.vimtex_view_skim_activate = 1
+    vim.g.vimtex_quickfix_open_on_warning = 0
 
     vim.keymap.set("n", "<leader>tt", function()
       require("which-key").show("\\")
